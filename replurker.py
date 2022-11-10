a57d5c8140c9bef76a2eeb007d88202391a0d1e7
Phidias Chiang <phidias@dt42ai.com>
2022-11-10 22:16:24 +0800

    Move code to main()

--- replurker.py
+++ replurker.py
@@ -17,16 +17,16 @@ def parse_args(args: List[str] = None) -> Any:
     return parser.parse_args()
 
 
-def is_replurkable(p: Dict, allow_anonymous: bool) -> bool:
+def is_replurkable(p: Dict, keyword: str, allow_anonymous: bool) -> bool:
     return (
         p["replurkable"]
         and not p["replurked"]
-        and args.keyword in p["content_raw"]
+        and keyword in p["content_raw"]
         and (allow_anonymous or not p["anonymous"])
     )
 
 
-def get_plurk_ids(plurk: PlurkAPI, keyword: str) -> List[Dict]:
+def get_plurk_ids(plurk: PlurkAPI, keyword: str, allow_anonymous: bool) -> List[Dict]:
     plurks = plurk.callAPI(
         "/APP/PlurkSearch/search",
         {"query": keyword},
@@ -36,26 +36,26 @@ def get_plurk_ids(plurk: PlurkAPI, keyword: str) -> List[Dict]:
     replurk_ids = []
     for p in plurks["plurks"]:
         logger.trace(json.dumps(p, indent=2))
-        if is_replurkable(p, allow_anonymous):
+        if is_replurkable(p, keyword, allow_anonymous):
             replurk_ids.append(p["plurk_id"])
 
     return replurk_ids
 
 
 def replurk(plurk: PlurkAPI, ids: List[int]) -> List[Dict]:
-    replurk = plurk.callAPI("/APP/Timeline/replurk", {"ids": json.dumps(replurk_ids)})
+    replurk = plurk.callAPI("/APP/Timeline/replurk", {"ids": json.dumps(ids)})
 
     return replurk["results"]
 
 
-if __name__ == "__main__":
+def main():
     args = parse_args()
     logger.debug(args)
     key_file = args.auth_key
     allow_anonymous = args.allow_anonymous
     plurk = PlurkAPI.fromfile(key_file)
 
-    replurk_ids = get_plurk_ids(plurk, args.keyword)
+    replurk_ids = get_plurk_ids(plurk, args.keyword, allow_anonymous)
     replurk_results = replurk(plurk, replurk_ids)
 
     logger.trace(json.dumps(replurk_results, indent=2, ensure_ascii=False))
@@ -69,3 +69,7 @@ if __name__ == "__main__":
             logger.warning(f"failed ids: {', '.join(map(str, failed_id))}")
     except KeyError as e:
         logger.error(f"Replurked id mismatch, can't get replurk status. Missed id: {e}")
+
+
+if __name__ == "__main__":
+    main()
